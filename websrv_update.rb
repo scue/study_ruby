3f130db7d5d295b2fb081009ec9e91a7ab0acf03
flyscue <flyscue@gmail.com>
2014-12-16 09:17:38 +0800

    简化了版本获取方法

--- websrv_update.rb
+++ websrv_update.rb
@@ -8,8 +8,7 @@ require 'expectr'
 user_password="fakesf"
 mysql_password="123456"
 emmi_dir="/home/fakesf/emmfsrv"
-emmi_curr='/home/fakesf/web_emmi_current_version.txt'
-#web_emmi_path="/home/fakesf/emmfsrv/R3/20141210/SSLEMM_M65_WEB_16508_20141210.emmi"
+emmi_curr="/emm/appversion"
 
 # 获得更新
 def find_emmi(emmi_dir)
@@ -25,28 +24,25 @@ def find_emmi(emmi_dir)
 end
 
 # 执行升级
-def emm_web_update(web_emmi_path, user_password, mysql_password, emmi_curr)
-    runcmd = "sudo %s" % web_emmi_path
+def emm_web_update(web_emmi_path, user_password, mysql_password)
+    mysql_sed="/home/fakesf/set_webconf.sh"
+    runcmd = "sudo %s && sudo %s" % [web_emmi_path, mysql_sed]
     exp = Expectr.new(runcmd, flush_buffer: true)
     exp.expect("[sudo]")
     exp.puts(user_password)
     exp.expect("Enter password:")
     exp.puts(mysql_password)
     exp.interact!(blocking: true)
-    File.open(emmi_curr, "w") do |f|
-        f.puts web_emmi_path
-    end
 end
 
 web_emmi_path=find_emmi(emmi_dir)
 if not File.exist?(emmi_curr)
-    File.open(emmi_curr, "w+") {}
-    emm_web_update(web_emmi_path,user_password,mysql_password,emmi_curr)
+    emm_web_update(web_emmi_path,user_password,mysql_password)
 else
-    curr_emmi_version = File.open(emmi_curr, "r").first.chop
-    if curr_emmi_version.eql? web_emmi_path
+    curr_emmi_version = File.open("/emm/appversion", "r").first.chop
+    if web_emmi_path.include?(curr_emmi_version)
         puts "当前版本[%s]已最新!" % curr_emmi_version
     else
-        emm_web_update(web_emmi_path,user_password,mysql_password,emmi_curr)
+        emm_web_update(web_emmi_path,user_password,mysql_password)
     end
 end
